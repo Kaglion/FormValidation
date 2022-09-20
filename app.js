49b07fbc082aa850939b516e8d6d4e49ccd400ab
kaglion <azanguembh@gmail.com>
2022-09-20 13:24:20 +0100

    Confirm password validation

--- app.js
+++ app.js
@@ -6,106 +6,123 @@ const allImg = document.querySelectorAll('.check-icon');
 const allLine = document.querySelectorAll('.line div');
 const allSpan = document.querySelectorAll('span');
 
-userName.addEventListener('input', (e) => {
+    userName.addEventListener('input', (e) => {
 
-    if(e.target.value.length >= 3 ){
-        allSpan[0].style.display = 'none'
-        allImg[0].style.display = 'inline';
-        allImg[0].src = 'ressources/check.svg'
-    }
-    else {
-        allSpan[0].style.display = 'inline';
-        allImg[0].style.display = 'inline';
-        allImg[0].src = 'ressources/error.svg' 
-    }
-})
-
-userEmail.addEventListener('input', e => {
-
-    const regexEmail = /\S+@\S+\.\S+/;
-
-    if(e.target.value.search(regexEmail) === 0){
-        allSpan[1].style.display = 'none'
-        allImg[1].style.display = 'inline';
-        allImg[1].src = 'ressources/check.svg'
-    }
-
-    else  if(e.target.value.search(regexEmail)  === -1){
-        allSpan[1].style.display = 'inline';
-        allImg[1].style.display = 'inline';
-        allImg[1].src = 'ressources/error.svg'
-    }
-})
-
-let inputValue;
-const specialChar = /[^a-zA-Z0-9]/; // Represents all characters that are not in the range from which the '^' is used.
-const letter = /[a-z]/i;
-const number = /[0-9]/
-
-let objValidation = {
-    symbol : 0,
-    letter : 0,
-    number : 0
-}
-
-userPassword.addEventListener('input', e => {
-
-    inputValue = e.target.value;
-
-    if (inputValue.search(specialChar) !== -1 ) {
-        objValidation.symbol = 1
-    }
-    if (inputValue.search(letter) !== -1 ) {
-        objValidation.letter = 1
-    }
-    if (inputValue.search(number) !== -1 ) {
-        objValidation.number = 1
-    }
-
-    if (e.inputType = "deleteContentBackward") {
-
-        if (inputValue.search(specialChar) === -1 ) {
-            objValidation.symbol = 0
+        if(e.target.value.length >= 3 ){
+            allSpan[0].style.display = 'none'
+            allImg[0].style.display = 'inline';
+            allImg[0].src = 'ressources/check.svg'
         }
-        if (inputValue.search(letter) === -1 ) {
-            objValidation.letter = 0
+        else {
+            allSpan[0].style.display = 'inline';
+            allImg[0].style.display = 'inline';
+            allImg[0].src = 'ressources/error.svg' 
         }
-        if (inputValue.search(number) === -1 ) {
-            objValidation.number = 0
-        }        
-    }
+    })
 
+    userEmail.addEventListener('input', e => {
 
-    let allTest = 3;
-    for (const property in objValidation) {
-        if (objValidation[property] > 0 ) {
-            allTest++;
+        const regexEmail = /\S+@\S+\.\S+/;
+
+        if(e.target.value.search(regexEmail) === 0){
+            allSpan[1].style.display = 'none'
+            allImg[1].style.display = 'inline';
+            allImg[1].src = 'ressources/check.svg'
         }
+
+        else  if(e.target.value.search(regexEmail)  === -1){
+            allSpan[1].style.display = 'inline';
+            allImg[1].style.display = 'inline';
+            allImg[1].src = 'ressources/error.svg'
+        }
+    })
+
+    let inputValue;
+    const specialChar = /[^a-zA-Z0-9]/; // Represents all characters that are not in the range from which the '^' is used.
+    const letter = /[a-z]/i;
+    const number = /[0-9]/
+
+    let objValidation = {
+        symbol : 0,
+        letter : 0,
+        number : 0
     }
 
-    if (allTest < 6) {
-        allSpan[2].style.display = 'inline';
-        allImg[2].style.display = 'inline';
-        allImg[2].src = 'ressources/error.svg';
-    }
-    else{
-        allSpan[2].style.display = 'none';
-        allImg[2].src = 'ressources/check.svg'
-    }
+    userPassword.addEventListener('input', e => {
+
+        inputValue = e.target.value;
+
+        if (inputValue.search(specialChar) !== -1 ) {
+            objValidation.symbol = 1
+        }
+        if (inputValue.search(letter) !== -1 ) {
+            objValidation.letter = 1
+        }
+        if (inputValue.search(number) !== -1 ) {
+            objValidation.number = 1
+        }
+
+        if (e.inputType = "deleteContentBackward") {
+
+            if (inputValue.search(specialChar) === -1 ) {
+                objValidation.symbol = 0
+            }
+            if (inputValue.search(letter) === -1 ) {
+                objValidation.letter = 0
+            }
+            if (inputValue.search(number) === -1 ) {
+                objValidation.number = 0
+            }        
+        }
+
+
+        let allTest = 3;
+        for (const property in objValidation) {
+            if (objValidation[property] > 0 ) {
+                allTest++;
+            }
+        }
+
+        if (allTest < 6) {
+            allSpan[2].style.display = 'inline';
+            allImg[2].style.display = 'inline';
+            allImg[2].src = 'ressources/error.svg';
+        }
+        else{
+            allSpan[2].style.display = 'none';
+            allImg[2].src = 'ressources/check.svg'
+        }
+        
+        if (inputValue.length <= 6) {
+            allLine[0].style.display = 'block';
+            allLine[1].style.display = 'none';
+            allLine[2].style.display = 'none';
+        } else if (inputValue.length > 6 && inputValue.length <= 9 ) {
+            allLine[0].style.display = 'block';
+            allLine[1].style.display = 'block';
+            allLine[2].style.display = 'none';
+        }
+        else if (inputValue.length > 9 ) {
+            allLine[0].style.display = 'block';
+            allLine[1].style.display = 'block';
+            allLine[2].style.display = 'block';
+        }     
+    })
+
     
-    if (inputValue.length <= 6) {
-        allLine[0].style.display = 'block';
-        allLine[1].style.display = 'none';
-        allLine[2].style.display = 'none';
-    } else if (inputValue.length > 6 && inputValue.length <= 9 ) {
-        allLine[0].style.display = 'block';
-        allLine[1].style.display = 'block';
-        allLine[2].style.display = 'none';
-    }
-    else if (inputValue.length > 9 ) {
-        allLine[0].style.display = 'block';
-        allLine[1].style.display = 'block';
-        allLine[2].style.display = 'block';
-    }
-     
-})
+
+    userPswConfirm.addEventListener('input', e => {
+        console.log(inputValue);
+
+        if (e.target.value === 0) {
+            allImg[3].style.display = 'inline';  
+            allImg[3].src = 'ressources/error.svg';
+        }
+        else if (e.target.value === inputValue ) {
+            allImg[3].src = 'ressources/check.svg';
+            allImg[3].style.display = 'inline';
+        }
+        else {
+            allImg[3].src = 'ressources/error.svg'
+        }
+    })
